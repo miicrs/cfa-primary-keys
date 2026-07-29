f9380995db829e2ead6af5722d63e61918edf18a
Carmen Mei <crjwm00@gmail.com>
2026-07-29 13:54:15 -0700

    created banks database + added bank information

--- SQL/banks.py
+++ SQL/banks.py
@@ -7,6 +7,38 @@ sql = connection.cursor()
 banks_table = 'CREATE TABLE IF NOT EXISTS ' \
     'banks (' \
     '"Name" TEXT,' \
-    '"Contact Information" TEXT' \
+    '"Phone number" TEXT' \
     ')'
-sql.execute(SELECT * FROM banks)
+sql.execute(banks_table);
+connection.commit();
+
+# ("", ""), \
+
+bank_info = '''INSERT INTO banks VALUES \
+    ("Bank of America", "800-432-1000"), \
+    ("JPMorgan Chase", "800-935-9935"), \
+    ("Citi Bank", "800-374-9700"), \
+    ("Wells Fargo", "800-869-3557"), \
+    ("TD Bank", "888-751-9000"), \
+    ("Capital One", "877-383-4802"), \
+    ("HSBC Bank", "800-898-5999"), \
+    ("USAA", "800-531-8722"), \
+    ("Fidelity", "800-343-3548"), \
+    ("Key Bank", "800-539-2968"), \
+    ("US Bank", "800-872-2657"), \
+    ("BECU", "800-233-2328"), \
+    ("Cathay Bank", "213-625-4791"), \
+    ("PNC", "888-762-2265"), \
+    ("SunTrust", "800-786-8787"), \
+    ("Barclays", "800-309-6191") \
+'''
+sql.execute(bank_info);
+
+connection.commit();
+
+sql.execute('SELECT * FROM banks')
+view = sql.fetchall()
+for row in view:
+    print(row)
+
+connection.close()
