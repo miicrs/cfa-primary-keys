9350f55c7159164f305ba6e493b43f1dbe7f60b2
Carmen Mei <crjwm00@gmail.com>
2026-08-03 16:13:22 -0700

    added selection feature + next button

---
+++ JavaScript/bank-selection.js
@@ -0,0 +1,7 @@
+
+const cards = document.querySelectorAll('.bank-card, .other-card');
+    cards.forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('selected');
+    });
+});
