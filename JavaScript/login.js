566ca767334a0884370ec8c508d4565a56bb763c
Carmen Mei <crjwm00@gmail.com>
2026-08-10 13:42:31 -0700

    login edits

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -30,7 +30,7 @@ form.addEventListener('submit', async function (event) {
 
 async function login(email, password) {
     try {
-        const res = await fetch(`${API}/login`, {
+        const res = await fetch(`${API_URL}/login`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
